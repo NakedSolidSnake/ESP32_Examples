d7a1f11c9bf79002750907f9e02450114ce7f45e
NakedSolidSnake <cristianosstec@gmail.com>
2018-12-17 21:59:15 -0200

    template

---
+++ 00-template/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+
+void app_main()
+{
+	nvs_flash_init();
+	/* Create Tasks here */	
+	
+}
